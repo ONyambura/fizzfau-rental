f1f7c71d077d59c31fda3bc70ac38ca0da1dfb2f
fizzfau <59974801+fizzfau@users.noreply.github.com>
2021-02-09 15:26:40 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {
-    ESXBilling = false, -- true yaptıktan sonra esx_billinginizi scripte göre düzenlemeniz gerek, eğer yeterince bilgiye sahip değilseniz false olarak kalsın
-    Fee = 300, -- saat başı kesilen ücret
+    ESXBilling = false, -- let it false if you dont know what are you doing
+    Fee = 300, -- per hour
     Locations = {
         ["South Side Rental"] = {
             coords = {
@@ -9,7 +9,7 @@ Config = {
                 heading = 100.0 
             },
             Vehicles = {
-                {name = "blista",label = "Blista", price = 100, max_time = 6 --[[ hours ]]},
+                {name = "blista",label = "Blista", price = 100, max_time = 6 --[[ hours ]]}, --max time means, if you reach the max time with rented car you need to pay fine
                 {name = "asea",label = "Asea", price = 200, max_time = 6 --[[ hours ]]},
                 {name = "intruder",label = "Intruder", price = 300, max_time = 6 --[[ hours ]]},
                 {name = "ratbike",label = "Rat Bike", price = 400, max_time = 6 --[[ hours ]]},
@@ -17,7 +17,7 @@ Config = {
                 {name = "sultan",label = "Sultan", price = 500, max_time = 6 --[[ hours ]]},
                 {name = "slamvan",label = "Slamvan", price = 650, max_time = 6 --[[ hours ]]},
                 {name = "virgo",label = "Virgo", price = 100, max_time = 6 --[[ hours ]]},
-                {name = "fugitive",label = "Fugitive", price = 1000, max_time = 6 --[[ hours ]]},
+                {name = "fugitive",label = "Fugitive", price = 1000, max_time = 6 --[[ hours  ]]},
             }
         },
     }
