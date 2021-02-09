c709540c94543c653731e8dc1c0fcdfffcefa52b
fizzfau <59974801+fizzfau@users.noreply.github.com>
2021-02-09 16:26:22 +0300

    Update job.lua

--- client/job.lua
+++ client/job.lua
@@ -1,128 +1,85 @@
 ESX = nil
-local ped, currentStore, Vehicle = nil, nil, nil
-Citizen.CreateThread(function()
-    while ESX == nil do
-        TriggerEvent('esx:getSharedObject', function(obj)
-            ESX = obj
-        end)
-        Citizen.Wait(0)
+VehiclesRented = {}
+TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+
+RegisterServerEvent("fizzfau-rental:rent")
+AddEventHandler("fizzfau-rental:rent", function(k, v, plate, store)
+    local player = ESX.GetPlayerFromId(source)
+    local identifier = player.identifier
+    if VehiclesRented[identifier] == nil then
+        VehiclesRented[identifier] = {
+            plate = plate,
+            time = os.time(),
+            max_time = v.max_time * 60 * 60 * 1000,
+            price = v.price,
+            store = store
+        }
+        TriggerClientEvent("fizzfau-rental:spawnVehicle", source, k, v, plate)
+        TriggerEvent("fizzfau-rental:startCounter", player)
+    else
+        TriggerClientEvent("notification", source, "Zaten kiralamış olduğun bir araç var, önce o aracı teslim et!")
     end
-    ped = PlayerPedId()
-    TriggerServerEvent("fizzfau-rental:getvehicle")
 end)
 
-Citizen.CreateThread(function()
-    while true do
-        local thread = 750
-        if ped ~= nil then
-            local coords = GetEntityCoords(ped)
-            for k,v in pairs(Config.Locations) do
-                local distance = #(coords - v.coords.rent)
-                if distance <= 5 then
-                    thread = 0
-                    text = "Araç Kirala"
-                    if distance <= 2.5 then
-                        text = "E - " ..text
-                        if IsControlJustReleased(0, 46) then
-                            OpenRentMenu(k, v)
+RegisterServerEvent("fizzfau-rental:giveback")
+AddEventHandler("fizzfau-rental:giveback", function(health)
+    local player = ESX.GetPlayerFromId(source)
+    local identifier = player.identifier
+    if VehiclesRented[identifier] ~= nil then
+        TriggerClientEvent("fizzfau-rental:delete", source, VehiclesRented[identifier].vehicle)
+        if health < 1000 then
+            player.removeMoney(Config.Fee * (1000 - health)/100)
+        elseif os.time() - VehiclesRented[identifier].time > VehiclesRented[identifier].max_time then
+            player.removeMoney(Config.Fee * max_time)
+        end
+        VehiclesRented[identifier] = nil
+    end
+end)
+
+RegisterServerEvent("fizzfau-rental:update")
+AddEventHandler("fizzfau-rental:update", function(vehicle, bool)
+    local player = ESX.GetPlayerFromId(source)
+    local identifier = player.identifier
+    if bool then
+        VehiclesRented[identifier] = nil
+        return
+    end
+    VehiclesRented[identifier].vehicle = vehicle    
+end)
+
+RegisterServerEvent("fizzfau-rental:getVehicle")
+AddEventHandler("fizzfau-rental:getVehicle", function()
+    local player = ESX.GetPlayerFromId(source)
+    local identifier = player.identifier
+    if VehiclesRented[identifier] ~= nil then
+        TriggerClientEvent("fizzfau-rental:client:getVehicle", source, VehiclesRented[identifier].vehicle)
+    end
+end)
+
+RegisterServerEvent("fizzfau-rental:startCounter")
+AddEventHandler("fizzfau-rental:startCounter", function(player)
+    local identifier = player.identifier
+    Citizen.CreateThread(function()
+        while true do
+            if VehiclesRented[identifier] ~= nil then
+                if player then
+                    local cash = player.getMoney()
+                    local bank = player.getAccount("bank").money
+                    if bank >= VehiclesRented[identifier].price then
+                        player.removeAccountMoney("bank", VehiclesRented[identifier].price)
+                    elseif cash >= VehiclesRented[identifier].price then
+                        player.removeMoney(VehiclesRented[identifier].price)
+                    else
+                        if Config.ESXBilling then
+                            TriggerEvent("esx_billing:sendBill", source, VehiclesRented[identifier].store, VehiclesRented[identifier].plate.. " plakalı kiralık araç", VehiclesRented[identifier].price)
+                            TriggerEvent("notification", source, "Bu aracın parasını karşılayamadığınız ücret fatura olarak kesildi!")
                         end
                     end
-                    DrawText3Ds(v.coords.rent, text)
                 end
+            else
+                break
             end
+            Citizen.Wait(60*60*1000)
         end
-        Citizen.Wait(thread)
-    end
-end)
-
-Citizen.CreateThread(function()
-    while true do
-        local thread = 750
-        if Vehicle ~= nil and currentStore ~= nil then
-            local coords = GetEntityCoords(Vehicle)
-            local distance = #(coords - Config.Locations[currentStore].coords.spawn)
-            if distance <= 5 then
-                thread = 0
-                text = "Aracı Bırak"
-                if distance <= 2.5 then
-                    text = "E - " ..text
-                    if IsControlJustReleased(0, 46) then
-                        GiveBackVehicle()
-                    end
-                end
-                DrawText3Ds(Config.Locations[currentStore].coords.spawn, text)
-            end
-        end
-        Citizen.Wait(thread)
-    end
-end)
-
-RegisterNUICallback("close", function()
-    SetNuiFocus(false, false)
-end)
-
-RegisterNUICallback("rent", function(data)
-    local plate = GeneratePlate()
-    local data = data.data
-    TriggerServerEvent("fizzfau-rental:rent", data.name, data, plate, currentStore)
-end)
-
-RegisterNetEvent("fizzfau-rental:spawnVehicle")
-AddEventHandler("fizzfau-rental:spawnVehicle", function(k, v, plate)
-    ESX.Game.SpawnVehicle(v.name, Config.Locations[currentStore].coords.spawn, Config.Locations[currentStore].coords.Heading, function(vehicle)
-        Vehicle = vehicle
-        TaskWarpPedIntoVehicle(ped, vehicle, -1)
-        SetVehicleNumberPlateText(vehicle, plate)
-
-        FreezeEntityPosition(ped, false)
-        SetEntityVisible(ped, true)
-        TriggerServerEvent("fizzfau-rental:update", vehicle)
     end)
 end)
-
-RegisterNetEvent("fizzfau-rental:delete")
-AddEventHandler("fizzfau-rental:delete", function(vehicle)
-    TaskLeaveVehicle(ped, vehicle, 0)
-    while IsPedInVehicle(ped, vehicle, true) do
-      Citizen.Wait(100)
-    end
-    Citizen.Wait(1000)
-    DeleteEntity(vehicle)
-end)
-
-RegisterNetEvent("fizzfau-rental:client:getvehicle")
-AddEventHandler("fizzfau-rental:client:getvehicle", function(vehicle)
-    Vehicle = vehicle
-end)
-
-function GiveBackVehicle()
-    local health = GetEntityHealth(Vehicle)
-    TriggerServerEvent("fizzfau-rental:giveback", health)
-    Vehicle = nil
-end
-
-function OpenRentMenu(k, v)
-    currentStore = k
-    SendNUIMessage({ key = k, value = v })
-    SetNuiFocus(true, true)
-end
-
-function DrawText3Ds(coords, text)
-    local onScreen,_x,_y=World3dToScreen2d(coords.x, coords.y, coords.z)
-    local px,py,pz=table.unpack(GetGameplayCamCoords())
-    SetTextScale(0.35, 0.35)
-    SetTextFont(4)
-    SetTextProportional(1)
-    SetTextColour(255, 255, 255, 215)
-    SetTextEntry("STRING")
-    SetTextCentre(1)
-    AddTextComponentString(text)
-    DrawText(_x,_y)
-    local factor = (string.len(text)) / 370
-	DrawRect(_x,_y+0.0125, 0.015+ factor, 0.03, 41, 11, 41, 68)
-	ClearDrawOrigin()
-end
-
-function GeneratePlate()
-	return string.upper('fizzfau ' ..math.random(100,999))
-end
